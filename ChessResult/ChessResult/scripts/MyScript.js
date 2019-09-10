f34048a95062cd34a0ef156251658117065e3468
15110150 <hoanghavinguyen@gmail.com>
2019-09-10 22:40:44 +0700

    add script

---
+++ ChessResult/ChessResult/scripts/MyScript.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    $('#pageSubmenu').on('click', 'li a', function () {
+        var url = $(this).data('request-url');
+        $.get(url, function (data) {
+            $('#content-data').html(data);
+        });
+    });
+});
+
+$(function () {
+    $('#staring-rank').click(function () {
+        var url = $(this).data('request-url');
+        $.get(url, function (data) {
+            $('#content-data').html(data);
+        });
+    });
+});
